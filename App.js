7c15d17d2ae9f635262fd403bb8cda9f67f619a6
rabah babaci <4rabah@gmail.com>
2020-05-18 10:24:15 -0700

    add the logo with the title positioned in the app

--- App.js
+++ App.js
@@ -3,15 +3,18 @@ import {
   useDimensions,
   useDeviceOrientation,
 } from "@react-native-community/hooks";
-import { View, ImageBackground, Text, StyleSheet } from "react-native";
+import { View, ImageBackground, Text, StyleSheet, Image } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <ImageBackground
         source={require("./images/background.jpg")}
-        style={styles.image}
-      ></ImageBackground>
+        style={styles.bgImage}
+      >
+        <Image source={require("./images/logo-red.png")} style={styles.logo} />
+        <Text style={styles.text}>Sell what You Don't Need</Text>
+      </ImageBackground>
     </View>
   );
 }
@@ -20,7 +23,18 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
-  image: {
+  bgImage: {
     flex: 1,
+  },
+  logo: {
+    top: 70,
+    alignSelf: "center",
+    width: 80,
+    height: 80,
+  },
+  text: {
+    top: 70,
+    alignSelf: "center",
+    fontSize: 12,
   },
 });
