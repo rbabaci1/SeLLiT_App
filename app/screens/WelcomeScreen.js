96e0a0bf3cdb788d396969662e28dacb62630818
rabah babaci <4rabah@gmail.com>
2020-05-18 10:55:00 -0700

    put the logo and the text under it in a parent container

--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, StyleSheet, ImageBackground, Image } from "react-native";
+import { View, StyleSheet, ImageBackground, Image, Text } from "react-native";
 
 const WelcomeScreen = () => {
   return (
@@ -7,7 +7,11 @@ const WelcomeScreen = () => {
       style={styles.background}
       source={require("../assets/background.jpg")}
     >
-      <Image style={styles.logo} source={require("../assets/logo-red.png")} />
+      <View style={styles.logoContainer}>
+        <Image style={styles.logo} source={require("../assets/logo-red.png")} />
+        <Text>Sell What You Don't Need</Text>
+      </View>
+
       <View style={styles.loginButton}></View>
       <View style={styles.registerButton}></View>
     </ImageBackground>
@@ -20,11 +24,14 @@ const styles = StyleSheet.create({
     justifyContent: "flex-end",
     alignItems: "center",
   },
+  logoContainer: {
+    alignItems: "center",
+    position: "absolute",
+    top: 70,
+  },
   logo: {
     width: 100,
     height: 100,
-    position: "absolute",
-    top: 70,
   },
   loginButton: {
     width: "100%",
