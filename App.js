74e7b6ed6cced5f72fc9b82ce6d152eb8a3227fe
rabah babaci <4rabah@gmail.com>
2020-05-18 10:37:03 -0700

    in app.js, import and render the welcome screen

--- App.js
+++ App.js
@@ -1,48 +1,8 @@
 import React from "react";
-import {
-  useDimensions,
-  useDeviceOrientation,
-} from "@react-native-community/hooks";
-import { View, ImageBackground, Text, StyleSheet, Image } from "react-native";
+
+import { View } from "react-native";
+import WelcomeScreen from "./app/screens/WelcomeScreen";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <ImageBackground
-        source={require("./app/assets/background.jpg")}
-        style={styles.bgImage}
-      >
-        <Image
-          source={require("./app/assets/logo-red.png")}
-          style={styles.logo}
-        />
-        <Text style={styles.text}>Sell what You Don't Need</Text>
-      </ImageBackground>
-
-      <View style={{ width: "100%", height: 50, backgroundColor: "orange" }} />
-      <View
-        style={{ width: "100%", height: 50, backgroundColor: "dodgerblue" }}
-      />
-    </View>
-  );
+  return <WelcomeScreen />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  bgImage: {
-    flex: 1,
-  },
-  logo: {
-    top: 70,
-    alignSelf: "center",
-    width: 80,
-    height: 80,
-  },
-  text: {
-    top: 70,
-    alignSelf: "center",
-    fontSize: 12,
-  },
-});
