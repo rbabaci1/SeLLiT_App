9d21c9f3e058b18e5f69d7604d7ca431e2a9d59d
rabah babaci <4rabah@gmail.com>
2020-05-18 10:04:47 -0700

    create the image folder and upload the home image

--- App.js
+++ App.js
@@ -3,36 +3,29 @@ import {
   useDimensions,
   useDeviceOrientation,
 } from "@react-native-community/hooks";
-import {
-  StyleSheet,
-  SafeAreaView,
-  Platform,
-  StatusBar,
-  View,
-} from "react-native";
+import { View } from "react-native";
 
 export default function App() {
-  const { landscape } = useDeviceOrientation();
-
   return (
-    <SafeAreaView style={[styles.container]}>
-      <View
-        style={{
-          backgroundColor: "dodgerblue",
-          width: "100%",
-          height: landscape ? "100%" : "30%",
-        }}
-      ></View>
-    </SafeAreaView>
+    <View
+      style={{
+        flex: 1,
+        flexDirection: "row",
+        justifyContent: "center",
+        // alignItems: "center",
+        flexWrap: "wrap",
+        alignContent: "center",
+      }}
+    ></View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#f7f7f7",
-    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
-    // alignItems: "center",
-    // justifyContent: "center",
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "#f7f7f7",
+//     paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+//     // alignItems: "center",
+//     // justifyContent: "center",
+//   },
+// });
