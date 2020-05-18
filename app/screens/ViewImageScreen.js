7b7d690e6db633f0ca0fa9f2a4cf8005169fbdb9
rabah babaci <4rabah@gmail.com>
2020-05-18 10:56:04 -0700

    define the ViewImageScreen

---
+++ app/screens/ViewImageScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const ViewImageScreen = () => {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  );
+};
+
+export default ViewImageScreen;
