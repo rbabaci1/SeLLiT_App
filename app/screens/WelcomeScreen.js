e129d8d842ebf10bf8d91f13c21290e03a0eedf2
rabah babaci <4rabah@gmail.com>
2020-05-18 10:40:36 -0700

    add two buttons to the WelcomeScreen

--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -1,18 +1,32 @@
 import React from "react";
-import { StyleSheet, ImageBackground } from "react-native";
+import { View, StyleSheet, ImageBackground } from "react-native";
 
 const WelcomeScreen = () => {
   return (
     <ImageBackground
       style={styles.background}
       source={require("../assets/background.jpg")}
-    ></ImageBackground>
+    >
+      <View style={styles.loginButton}></View>
+      <View style={styles.registerButton}></View>
+    </ImageBackground>
   );
 };
 
 const styles = StyleSheet.create({
   background: {
     flex: 1,
+    justifyContent: "flex-end",
+  },
+  loginButton: {
+    width: "100%",
+    height: 70,
+    backgroundColor: "#fc5c65",
+  },
+  registerButton: {
+    width: "100%",
+    height: 70,
+    backgroundColor: "#4ecdc4",
   },
 });
 
