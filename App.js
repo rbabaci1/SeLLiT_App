3eb987bfc6e385441a80bca1b16cd47d31d73f5a
rabah babaci <4rabah@gmail.com>
2020-05-18 11:39:31 -0700

    create the View Image with buttons place holders

--- App.js
+++ App.js
@@ -2,7 +2,9 @@ import React from "react";
 
 import { View } from "react-native";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
+import ViewImageScreen from "./app/screens/ViewImageScreen";
 
 export default function App() {
-  return <WelcomeScreen />;
+  // return <WelcomeScreen />;
+  return <ViewImageScreen />;
 }
--- app/screens/ViewImageScreen.js
+++ app/screens/ViewImageScreen.js
@@ -1,12 +1,47 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Image, StyleSheet } from "react-native";
 
 const ViewImageScreen = () => {
   return (
-    <View>
-      <Text></Text>
+    <View style={styles.container}>
+      <View style={styles.closeIcon} />
+      <View style={styles.deleteIcon} />
+
+      <Image
+        resizeMode='contain'
+        style={styles.image}
+        source={require("../assets/chair.jpg")}
+      />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#000",
+  },
+
+  closeIcon: {
+    width: 50,
+    height: 50,
+    position: "absolute",
+    top: 40,
+    left: 30,
+    backgroundColor: "#fc5c65",
+  },
+  deleteIcon: {
+    width: 50,
+    height: 50,
+    position: "absolute",
+    top: 40,
+    right: 30,
+    backgroundColor: "#4ecdc4",
+  },
+  image: {
+    height: "100%",
+    width: "100%",
+  },
+});
 
 export default ViewImageScreen;
