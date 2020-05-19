5b86d55c9412957d46ceadb213d46ed56f0ae46b
rabah babaci <4rabah@gmail.com>
2020-05-19 09:23:44 -0700

    clean the App component

--- App.js
+++ App.js
@@ -1,10 +1,17 @@
 import React from "react";
+import { View, StyleSheet } from "react-native";
 
-import { View } from "react-native";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
 
 export default function App() {
-  // return <WelcomeScreen />;
-  return <ViewImageScreen />;
+  return <View style={styles.container}></View>;
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
