28d57b94e2f79efe5d0d1df66f8c0ecc0af80100
rabah babaci <4rabah@gmail.com>
2020-06-17 11:07:57 -0700

    refactor AppTextInput to use the default styles

--- app/components/AppTextInput.js
+++ app/components/AppTextInput.js
@@ -1,7 +1,9 @@
 import React from "react";
 import { View, TextInput, StyleSheet, Platform } from "react-native";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
+
 import colors from "../config/colors";
+import defaultStyles from "../config/styles";
 
 export default function AppTextInput({ icon, ...rest }) {
   return (
@@ -29,12 +31,6 @@ const styles = StyleSheet.create({
     padding: 15,
     marginVertical: 10,
   },
-  icon: {
-    marginRight: 10,
-  },
-  textInput: {
-    fontSize: 18,
-    fontFamily: Platform.OS === "android" ? "Roboto" : "Avenir",
-    color: colors.dark,
-  },
+  icon: { marginRight: 10 },
+  textInput: defaultStyles.text,
 });
